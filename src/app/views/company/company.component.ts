598cf8365694394aa74fadcc2454607e74ef4a2b
RNOre <92789250+RNOre@users.noreply.github.com>
2025-04-08 23:19:41 +0500

    company fix

--- src/app/views/company/company.component.ts
+++ src/app/views/company/company.component.ts
@@ -1,13 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {FilterInterface} from "../../interfaces/global";
+import {FilterInterface, PaginationMetaInterface} from "../../interfaces/global";
 import {CompanyInterface, TariffInterface, UserInterface} from "../../interfaces/billing";
 import {cilCheckAlt, cilX} from "@coreui/icons";
 import {DatePipe} from "@angular/common";
 import {IconDirective} from "@coreui/icons-angular";
 import {TableDirective} from "@coreui/angular";
 import {OrderWidgetComponent} from "../order-widget/order-widget.component";
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
+import {PaginationDirective} from "../../directives/pagination.directive";
 
 @Component({
   selector: 'app-company',
@@ -16,27 +17,21 @@ import {Router} from "@angular/router";
     IconDirective,
     TableDirective,
     OrderWidgetComponent,
+    PaginationDirective,
+    RouterLink,
   ],
   templateUrl: './company.component.html',
   standalone: true,
   styleUrl: './company.component.scss'
 })
 export class CompanyComponent implements OnInit {
-  filter: FilterInterface = {
-    filter: {
-      search: ''
-    },
-    order: {
-      createdAt: 'asc'
-    },
-    pagination: {
-      limit: 10,
-      offset: 0
-    }
+  meta: PaginationMetaInterface = {
+    currentPage: 1,
+    perPage: 10,
+    currentCount: 0
   };
 
   companyData!: CompanyInterface[];
-  userData!: UserInterface[];
   icons = {cilCheckAlt, cilX}
   tab = 'company';
 
@@ -45,31 +40,37 @@ export class CompanyComponent implements OnInit {
 
   ngOnInit() {
     this.getCompanyList();
-    this.getUserList();
   }
 
   getCompanyList() {
-    this.$http.post('http://82.97.241.8:8083/admin/api/v1/companies/filter', this.filter)
+    const filter: FilterInterface = {
+      filter: {
+        search: ''
+      },
+      order: {
+        createdAt: 'asc'
+      },
+      pagination: {
+        "limit": this.meta.perPage,
+        "offset": (this.meta.currentPage - 1) * this.meta.perPage
+      }
+    };
+    this.$http.post('http://82.97.241.8:8083/admin/api/v1/companies/filter', filter)
       // @ts-ignore
       .subscribe((res: { data: { items: TariffInterface[], total: number } }) => {
         // @ts-ignore
         this.companyData = res.data.items;
+        this.meta.totalCount = res.data.total;
+        this.meta.currentCount = this.companyData?.length || 0;
       })
   }
-
-  getUserList() {
-    this.$http.post<{
-      data: { items: UserInterface[], total: number }
-    }>('http://82.97.241.8:8083/admin/api/v1/users/filter', this.filter)
-      .subscribe({
-        next: (res) => {
-          this.userData = res.data.items;
-        }
-      })
-  }
-
   navigateToCompany(id: string) {
     this.$router.navigate(['company', id])
       .then();
+  }
+
+  pageChange(page: number) {
+    this.meta.currentPage = page;
+    this.getCompanyList();
   }
 }
