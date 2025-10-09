87e36771850999ed5eeb16dad542faa95763f160
RNOre <92789250+RNOre@users.noreply.github.com>
2025-10-09 20:20:05 +0500

    восстановление пароля

--- src/app/views/pages/login/login.component.ts
+++ src/app/views/pages/login/login.component.ts
@@ -26,9 +26,24 @@ import {isAdmin} from "../../../core/global";
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
   standalone: true,
-  imports: [ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, IconDirective, FormControlDirective, ButtonDirective, NgStyle, ReactiveFormsModule, FormsModule, JsonPipe, ToastComponent, ToastHeaderComponent, ToastBodyComponent]
+  imports: [
+    ContainerComponent,
+    RowComponent,
+    ColComponent,
+    CardGroupComponent,
+    CardComponent,
+    CardBodyComponent,
+    FormDirective,
+    InputGroupComponent,
+    InputGroupTextDirective,
+    IconDirective,
+    FormControlDirective,
+    ButtonDirective,
+    ReactiveFormsModule,
+    FormsModule
+    ]
 })
-export class LoginComponent{
+export class LoginComponent {
 
   is_visible = false;
 
@@ -39,7 +54,15 @@ export class LoginComponent{
     }
   )
 
+  email = new FormControl('', [Validators.required, Validators.email]);
+  otp = new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(4)]);
+
   isSuperAdmin = false;
+  forgetPass = false;
+  step: 0 | 1 | 2 = 0; // 0 - ввод почты, 1 - вод кода, 2 - ввод нового пароля
+  showPassword = false;
+  newPassword = new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(64)]);
+  newPasswordRepeat = new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(64)]);
 
   constructor(
     private $http: HttpClient,
@@ -50,7 +73,7 @@ export class LoginComponent{
   login() {
     if (this.isSuperAdmin) {
       this.$http
-    .post<{data:{token: string}}>('auth', this.authData.value)
+        .post<{ data: { token: string } }>('auth', this.authData.value)
         .subscribe({
           next: (res) => {
             localStorage.setItem('isAdmin', 'true');
@@ -64,16 +87,28 @@ export class LoginComponent{
       return;
     }
     this.$http
-      .post<{data:{token: string}}>(env.host + 'auth', this.authData.value)
+      .post<{ data: { token: string } }>(env.host + 'auth', this.authData.value)
       .subscribe({
         next: (res) => {
           localStorage.removeItem('isAdmin');
           localStorage.setItem('token', res.data.token);
-          this.$router.navigate([isAdmin()?'home': 'main-legal']).then();
+          this.$router.navigate([isAdmin() ? 'home' : 'main-legal']).then();
         },
         error: () => {
           this.is_visible = true;
         }
       });
+  }
+
+  sendCode() {
+    this.step = 2;
+  }
+
+  getCode() {
+    this.step = 1;
+  }
+
+  updatePassword() {
+    //
   }
 }
